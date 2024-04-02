eeed19231e7e4e89608c55183649cb64405c0721
Jens Böckmann <jensboe@outlook.de>
2024-04-02 20:02:50 +0200

    Anoter try

--- backend/firemark/settings.py
+++ backend/firemark/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -70,18 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'firemark.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -112,11 +94,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT  = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = '/media/'

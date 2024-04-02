1a631b3a7ded3718f1a8325dcf7e6ff794c3635d
Jens Böckmann <jensboe@outlook.de>
2024-04-02 21:00:31 +0200

    Improve doc in pessenger file

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -3,29 +3,28 @@ APP_SPECIFIC_VENV = "firemark"
 PYTHON_VERSION = "python3.12"
 MINICONDA_ROOT = "/miniconda3"
 """
-Miniconda3 installer doesn't work.
+Miniconda3 installer doesn't work always.
 Miniconda3-4.5.11-Linux-x86_64.sh is a working version
 
-os.environ["HOME"] is "~/"
-Pessenger isn't the user. Therefor "/ becomes "/var/www/vhosts/hosting*user*.*server*.netcup.net/"
+os.environ["HOME"] is the path which the normal ssh user sees as "~/".
+But Pessenger isn't the user. Therefor "/" becomes "/var/www/vhosts/hosting*user*.*server*.netcup.net/"
 
-Create your app specific conda venv with 'conda create --name APP_SPECIFIC_VENV python=X'. X Specifies the python versio
+Create your app specific conda venv with 'conda create --name APP_SPECIFIC_VENV python=X'. X Specifies the python version
+
+This file in a recursive way.
+'Some' interpreter will call this file.
+If it doesnt match our defined interpreter it will call the defined interpreter.
+If it match the defined interpreter it will run the normal application code.
 """
 
 import sys, os
 INTERP = os.environ["HOME"]+MINICONDA_ROOT+"/envs/"+APP_SPECIFIC_VENV+"/bin/"+PYTHON_VERSION
 
-"""
-This works in a recursive way.
-'Some' interpreter will call this file.
-If it doesnt match our defined interpreter it will call the defined interpreter.
-If It match the defined interpreter it will run the normal application code.
-"""
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
 """
-application specific code
+application specific code starts here
 """
 
 from django.core.wsgi import get_wsgi_application
